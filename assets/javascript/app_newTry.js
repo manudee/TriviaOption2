abb0cd430af7b08d31d81bb58992bbdb1d323602
Manasi <manudee@gmail.com>
2017-10-04 21:12:23 -0400

    Trivia 2 major changes done updated

--- assets/javascript/app_newTry.js
+++ assets/javascript/app_newTry.js
@@ -1,75 +1,75 @@
 var triviaTime = 15;
 var breakTime = 5;
 var questionDone = false;
-
-
+ 
+//counters to keep tracj of answers
 var correctAnswers = 0;
 var incorrectAnswers = 0;
 var unanswered = 0;
-
+ 
 var currAns ;
-
+ 
 //booleans to keep track of correct, incorrect and unanswered questions for displaying results after each ans click
 var bool_correctAnswer = false;
 var bool_incorrectAnswer = false;
 var bool_unanswered = false;
-
+ 
 var trueAnswer;
-
-
+ 
+ 
 var currentQuestion = 0;
 var quizDone = false;
 var breakDone = false;
 var ans;
-
+ 
 //global object for trivia questions
 var triviaQ = [
-
-
+ 
+ 
 {
   question: "What is the national language of India?",
   answers: ["English","Hindi","Tulu","Nepali"],
   correctAnswer: 1
-
+ 
 },
-
+ 
 {
   question: "The oldest parliament in the world belongs to what country?",
   answers: ["Iceland","Netherlands","Scotland","Ireland"],
   correctAnswer: 0
-
+ 
 },
-
+ 
 {
   question: "In what year did Fidel Castro die?",
   answers: ["2015","2014","2016","2017"],
   correctAnswer: 2
-
+ 
 },
-
+ 
 {
   question: "HTML and CSS are computer languages used to create what?",
   answers: ["Bugs","Games","Toys","WebSites"],
   correctAnswer: 3
-
+ 
 },
-
+ 
 {
   question: "In our solar system which two planets rotate clockwise?",
   answers: ["Mars and Saturn","Neptune and pluto","Venus & Uranus","None of the above"],
-
+ 
   correctAnswer: 2
-
+ 
 }
-
-
-
+ 
+ 
+ 
 ]
-
+ 
 var  totalQuestions = triviaQ.length;
-
-
-
+ 
+ 
+ 
 function startGame() {
               console.log("questionDone " + questionDone);
               if(!questionDone){
@@ -78,55 +78,67 @@ function startGame() {
               $("#triviaTime").show();
   }
 }
-
-
+ 
+ 
 function stopGame() {
-
+ 
                 clearInterval(triviaId);
-
+ 
+ 
 }
-
-
- function decrement() {
-  
+ 
+ 
+function decrement() {
+ 
                 triviaTime--;
                 $("#triviaTime").empty();
                 $("#triviaTime").html("<h2>" + "TimeRemaining:" +  triviaTime + "</h2>");
-                
+               
                 if(triviaTime === 0)
                   stopGame();
-
+ 
 }
-
-
+ 
+ 
 function startBreak(){
     if(!breakDone){
                   breakTimeID = setInterval(decrementBreak, 1000);
                   breakDone = true;
-              
-
-
+             
+ 
+ 
 }
 }
-
-
- function decrementBreak() {
-  
-                if(breakTime === 0)
+ 
+ 
+function decrementBreak() {
+ 
+   breakTime--;
+                $("#breakTime").empty();
+                $("#breakTime").html("<h2>" + "breakTimeRemaining:" +  breakTime + "</h2>");
+ 
+                if(breakTime === 0){
                   stopBreak();
-
+                  
+                  breakDone = false;
+                  breakTime = 5;
+                  triviaTime = 15;
+                  questionDone = false;
+                  createQuiz();
+                  startGame();
+                  }
 }
-
-
-
+ 
+ 
+ 
 function stopBreak() {
-
+ 
                 clearInterval(breakTimeID);
-
+ 
 }
-
-
- function reset(){
+ 
+ 
+function reset(){
                 questionDone = false;
                 triviaTime = 15;
                 correctAnswers = 0;
@@ -141,96 +153,92 @@ function stopBreak() {
                 $("#questions").empty();
                 $("answers").empty();
 }
-
-
-
-
-
-
-
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 function createQuiz(){
-   
-
-                
-                console.log("Total  Questions before create Quiz " +  totalQuestions);
-                console.log("Current Question before create Quiz " +  currentQuestion);
-
+  
+ 
                
+ 
+              
                  $("#startGame").hide();
-
-
+ 
+ 
                  console.log("Current Question is" + triviaQ[currentQuestion].question);
-         
-
+        
+ 
                  $("#question").empty();
                  $("#question").append('<h2>' + triviaQ[currentQuestion].question + '</h2>');
-    
-
+   
+ 
                   $("#answers").empty();
                   console.log("Answers length for current question " + currentQuestion + " is " + triviaQ[currentQuestion].answers.length);
                   for (var i = 0; i < triviaQ[currentQuestion].answers.length; i++) {
-
+ 
                       var ans =$('<div id=answer'+currentQuestion+i+' name=answer'+ currentQuestion +'>' + triviaQ[currentQuestion].answers[i] +'</div>').addClass("answers col-md-offset-5 game").appendTo("#answers");
-                      
+                     
                       console.log("Creating Quiz");
-                      console.log(ans);
-
+                      //console.log(ans);
+ 
                   }
-           
-
-
+          
+ 
+ 
                 console.log("Total  Questions after create Quiz " +  totalQuestions);
                 console.log("Current Question after create Quiz " +  currentQuestion);
-
-
-
+ 
+ 
+ 
 }
-
-
-
+ 
+ 
+ 
 function checkAnswer(){
-
-
+ 
+ 
 console.log("IN CHECK ANSWER");
-
+ 
 console.log("answerValue " + answerValue);
-
+ 
 currAns = triviaQ[currentQuestion].answers[triviaQ[currentQuestion].correctAnswer];
-
+ 
 console.log("currAns " + currAns);
-
-
+ 
+ 
 if(answerValue === currAns){
-        correctAnswers++;  
+        correctAnswers++; 
         console.log("That's Correct!");
         bool_correctAnswer = true;
-
+ 
 }
-
- else if(!answerValue){
+ 
+else if(!answerValue){
           unanswered++;
           console.log("Nope You did not answer! Correct answer was " + currAns);
           bool_unanswered = true;
          }
- else {
+else {
           incorrectAnswers++;
           console.log("Nope that is incorrect! Correct answer was " + currAns);
           bool_incorrectAnswer = true;
       }
-
+ 
 }
-
+ 
 function displayStats(){
-
-              startBreak();
-
-              var newDiv = $('div');
-              
-             
-
-
+ 
+            
+ 
+            
+ 
+ 
               if(bool_correctAnswer){
-                
+               
                  $("#question").empty();
                  $("#answers").empty();
                  $("#results").empty();
@@ -239,9 +247,9 @@ function displayStats(){
                 "<div><h2>You Answered " + answerValue + "</h2></div>",
                 "<div><h2>Your answer matches the correct Answer " + currAns + "</h2></div>");
                }
-
+ 
               if(bool_unanswered){
-
+ 
                  $("#question").empty();
                  $("#answers").empty();
                  $("#results").empty();
@@ -249,9 +257,9 @@ function displayStats(){
                 "<div><h2>You did not Answer !!</h2></div>",
                 "<div><h2>The correct Answer was " + currAns + "</h2></div>");
               }
-
+ 
               if(bool_incorrectAnswer){
-
+ 
                  $("#question").empty();
                  $("#answers").empty();
                  $("#results").empty();
@@ -260,75 +268,75 @@ function displayStats(){
                 "<div><h2>You Answered " + answerValue + "</h2></div>",
                 "<div><h2>The correct Answer was " + currAns + "</h2></div>");
               }
-
-
-
-
-
-
+ 
+ 
+ 
+ 
+ 
+ 
 }
-
-
-
-
-
-
-
-
-
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 $(document).ready(function(){
-
-
-
-
+ 
+ 
+ 
+ 
    $("#startGame").on("click", function(){
-
+ 
     //on page load click on start game to start the game time and create quiz for 1st question.
+                   
+ 
                     startGame();
                     createQuiz();
-                  
-                     //currentQuestion++;
-                     //totalQuestions--;
-                     console.log("Total  Questions on ANS click " +  totalQuestions);
-                     console.log("Current Question on ANS click " +  currentQuestion);              
-        
-
-
-
- $("body").on("click", ".answers", function(){
-
+                 
+ 
+ 
+ 
+ 
+$("body").on("click", ".answers", function(){
+ 
+ 
+console.log("I CLICKED an answer");
+                if(totalQuestions!=0){
+                  console.log("Stopping Game");
+                  stopGame();
+                  startBreak();
+                
+              
+ 
+ 
                 answerValue = $(this).text();
                 console.log("answerValue: " + answerValue);
                 checkAnswer();
                 displayStats();
+ 
+              //   if(breakTime === 0){
+              //        createQuiz();
+              //        startGame();
+              // }
+ 
+ 
+                console.log("BreakTime is " + breakTime);
+                console.log("startGame flag is " + questionDone);
                 currentQuestion++;
                 totalQuestions--;
-                
-               
-                createQuiz();
- });
-
-/*
-                     if(totalQuestions > 0){
-                      //call a function to calculate stats
-                      //call the function to display results and image
-                      //stop the timer for currentQuestion
-                      //set the triviaTime to 15
-
-
-                     }
-
-                     else if (totalQuestions===0){
-                      //display Stats() call function
-                      //
-
-                     }
-*/
-
-
-
-
-              // });
+              }
+ 
+ 
+              else
+              alert("GAME OVER!!");  
+      });   
+ 
+ 
       });
-
+ 
 });
+ 
